76b3b0013bd3a63842fa17d663c4dcee19fc4b0c
Raushan Kumar <152690959+Raushan7549@users.noreply.github.com>
2026-02-12 13:11:56 +0530

    Add files via upload

---
+++ Odd_Even_Linked_List.cpp
@@ -0,0 +1,28 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==nullptr || head->next == nullptr) return head;
+        ListNode* odd = head;
+        ListNode* oddhead = head;
+        ListNode* even = head->next; 
+        ListNode* evenhead = head->next; 
+        head = head->next->next;
+        while(head!=nullptr){
+            odd->next = head;
+            even->next = head->next;
+            odd = odd->next;
+            even = even->next;
+            head = head->next ? head->next->next : nullptr;
+        }
+        odd->next = evenhead;
+        return oddhead;
+    }
+};
